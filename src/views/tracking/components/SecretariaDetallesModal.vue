71db90713b33aac1858f768962f2290199f7e37e
DevSoft-RECO <ronycardonaordonez@gmail.com>
2026-02-06 11:57:43 -0600

    correcion, adjuntar contrato cuando el expediente este ya sea en ele satdo 3,4, o 5

--- src/views/tracking/components/SecretariaDetallesModal.vue
+++ src/views/tracking/components/SecretariaDetallesModal.vue
@@ -171,7 +171,15 @@
                 <div class="w-full md:w-auto border-l border-gray-300 dark:border-gray-600 mx-2 hidden md:block"></div>
 
                 <!-- Acción: Adjuntar Contrato (Visible si estado 3 y no tiene contrato) -->
-                 <button v-if="currentState === 3 && !numeroContrato" @click="handleAction('adjuntar-contrato')" class="px-5 py-2.5 text-white bg-sky-600 rounded-lg hover:bg-sky-700 shadow-md transition flex items-center gap-2">
+                 <!-- <button v-if="currentState === 3 && !numeroContrato" @click="handleAction('adjuntar-contrato')" class="px-5 py-2.5 text-white bg-sky-600 rounded-lg hover:bg-sky-700 shadow-md transition flex items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                    </svg>
+                    Agregar No. Contrato
+                </button> -->
+                <button v-if="[3, 4, 5].includes(currentState) && !numeroContrato" 
+                        @click="handleAction('adjuntar-contrato')" 
+                        class="px-5 py-2.5 text-white bg-sky-600 rounded-lg hover:bg-sky-700 shadow-md transition flex items-center gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                     </svg>
