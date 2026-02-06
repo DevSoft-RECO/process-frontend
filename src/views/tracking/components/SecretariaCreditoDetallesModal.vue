86913c9bafcbc10e58f6783146d3ac4f63fa40e2
DevSoft-RECO <ronycardonaordonez@gmail.com>
2026-02-06 14:36:00 -0600

    boton aceptar se desactiva cuando se acepta el expediente por secretaria de credito

--- src/views/tracking/components/SecretariaCreditoDetallesModal.vue
+++ src/views/tracking/components/SecretariaCreditoDetallesModal.vue
@@ -148,7 +148,7 @@
                 <div class="w-full md:w-auto border-l border-gray-300 dark:border-gray-600 mx-2 hidden md:block"></div>
 
                  <!-- Acción: Aceptar / Validar (Visual) -->
-                 <button @click="handleAction('aceptar')" class="px-5 py-2.5 text-white bg-green-600 rounded-lg hover:bg-green-700 shadow-md transition flex items-center gap-2">
+                 <button v-if="expediente?.seguimientos?.[0]?.id_estado === 5" @click="handleAction('aceptar')" class="px-5 py-2.5 text-white bg-green-600 rounded-lg hover:bg-green-700 shadow-md transition flex items-center gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
                     </svg>
