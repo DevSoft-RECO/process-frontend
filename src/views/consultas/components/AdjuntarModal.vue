c2b73c3cef13b5b0cf1de422f397d2f396d48e59
DevSoft-RECO <ronycardonaordonez@gmail.com>
2026-02-22 16:21:36 -0600

    actualizacion de texto correcto

--- src/views/consultas/components/AdjuntarModal.vue
+++ src/views/consultas/components/AdjuntarModal.vue
@@ -4,7 +4,7 @@
             <!-- Header with Close Button -->
             <div class="flex justify-between items-center p-4 border-b border-gray-100 dark:border-gray-700 shrink-0">
                 <h2 class="text-xl font-bold text-gray-900 dark:text-white">
-                    Adjuntar Información
+                    Adjuntar Garantias
                 </h2>
                 <button @click="close" class="text-gray-400 hover:text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-700 p-2 rounded-full transition">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
