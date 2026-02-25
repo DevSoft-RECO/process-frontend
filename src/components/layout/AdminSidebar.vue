dd4ea0e79c64afd01396b1883848908295a6c4ab
DevSoft-RECO <ronycardonaordonez@gmail.com>
2026-02-24 18:18:59 -0600

    cambios en mis apps

--- src/components/layout/AdminSidebar.vue
+++ src/components/layout/AdminSidebar.vue
@@ -610,7 +610,7 @@ const menuItems = computed(() => {
             id: 'reportes-gerencia',
             label: 'Reportes CSV',
             iconSvg: '<path stroke-linecap="round" stroke-linejoin="round" d="M3 10h18M3 14h18m-9-4v8m-7 0h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />',
-            permission: 'Seguimiento_Principal || Consolidado_Agencias || Rendimiento_Personal || Estado_Garantías_Reales || Archivos_Administrativos || Retiro_Garantías || Confirmación_Física',
+            permission: 'Seguimiento_Principal || Consolidado_Agencias || Rendimiento_Personal || Estado_Garantías_Reales || Archivos_Administrativos || Retiro_Garantías_Fisica || Confirmación_Física',
             route: '/admin/reportes'
         },
 
