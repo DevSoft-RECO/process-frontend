0a350ddfc2fc50e3f3e066b77b378c05400417d6
DevSoft-RECO <ronycardonaordonez@gmail.com>
2026-03-23 18:05:38 -0600

    actualizacion de etiqueta

--- src/components/layout/AdminHeader.vue
+++ src/components/layout/AdminHeader.vue
@@ -100,7 +100,7 @@
         <button
           @click="handleReturnToMother"
           class="ml-1 p-2 rounded-lg text-red-400 hover:bg-red-500 hover:text-white transition"
-          title="Salir al portal"
+          title="Regresar al portal"
         >
           <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
